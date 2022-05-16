b7c948ee0afea9e91456688dbca96e232152a726
maltyyev <amk0794@gmail.com>
2022-05-16 21:38:32 +0300

    first commit

---
+++ Assets/Scripts/SampleCube.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SampleCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I am a cube!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
