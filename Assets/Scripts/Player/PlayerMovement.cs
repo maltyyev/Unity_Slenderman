ad93825dbb224f4c40241f34f7ba1c4357970165
maltyyev <amk0794@gmail.com>
2022-05-21 19:06:50 +0300

    fix player gravity

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,34 +1,43 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private InputManager _inputManager;
     [SerializeField, Min(1f)] private float _movementSpeed;
-
-    private CharacterController _characterController;
+    [SerializeField] private Transform _camera;
 
     #region Movement variables
 
     private Vector2 _movementInputVector;
-    private Vector3 _movementDirection;
+    private Vector3 _movementDirection, _projectedCameraDirection;
+    private Quaternion _movementRotation;
     private float _xAxis, _yAxis;
 
     #endregion
 
     private void Start()
     {
-        _characterController = GetComponent<CharacterController>();
+        _inputManager.Controls.Player.Movement.performed += OnMove;
+        _inputManager.Controls.Player.Movement.canceled += OnMove;
+    }
+
+    private void OnMove(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        _movementInputVector = obj.ReadValue<Vector2>();
+
+        _xAxis = _movementInputVector.x;
+        _yAxis = _movementInputVector.y;
     }
 
     private void FixedUpdate()
     {
-        _movementInputVector = _inputManager.GetMovementInput();
-
-        _xAxis = _movementInputVector.x;
-        _yAxis = _movementInputVector.y;
-
         _movementDirection = Vector3.forward * _yAxis + Vector3.right * _xAxis;
-        _characterController.Move(transform.TransformDirection(_movementDirection) * Time.fixedDeltaTime * _movementSpeed);
+
+        _projectedCameraDirection = Vector3.ProjectOnPlane(_camera.forward, Vector3.up);
+        _movementRotation = Quaternion.LookRotation(_projectedCameraDirection);
+
+        _movementDirection = (_movementRotation * _movementDirection).normalized;
+
+        transform.position += _movementDirection * Time.fixedDeltaTime * _movementSpeed;
     }
 }
